c64db68e0b84687b24f5691a7081b8b9bde22673
icha <m.ariefsaukani.ms@gmail.com>
2025-06-19 06:20:40 +0700

    create sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -24,12 +24,15 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "wrong password"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 36000000); // 1 hour
     res
       .cookie("access_token", token, {
         httpOnly: true,
+        expires: expiryDate,
       })
       .status(200)
-      .json(validUser);
+      .json(rest);
   } catch (error) {
     next(error);
   }
